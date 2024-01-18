ce17510367eb0057301e1ec4dd696f0974f0a83a
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2024-01-18 10:19:49 +0100

    Update convert_code_blocks.py
    Added capability of admonition block conversion

--- convert_code_blocks.py
+++ convert_code_blocks.py
@@ -8,6 +8,8 @@ def code_block_converter(S):
     if not isinstance(S, list):
         raise Exception('The input needs to be a list!')
     
+    begin_text_0 = '\\begin{minted}'
+    end_text_0 = '\end{minted}'
 
     counter = 0
     S1 = []
@@ -16,14 +18,37 @@ def code_block_converter(S):
         s1 = s
         if s1.startswith(code_block_obsidian):
             if counter%2 == 0:
+                # we are at the start
                 language = s1.replace(code_block_obsidian, '').lstrip().rstrip()
+
                 if language:
-                    language_additive = '{' + language + '}'
+                    IS_ADMONITION_BLOCK = language.startswith('ad-')
+                    if not IS_ADMONITION_BLOCK:
+                        language_additive = '{' + language + '}'
+                        begin_text =  begin_text_0
+                        end_text = end_text_0
+                    else:
+                        language_additive = ''
+                        kind_of_block = language.replace('ad-', '').strip()
+                        if kind_of_block == 'warning':
+                            color = 'red'
+                            colupper = 'white'
+                        else:
+                            color = 'white'
+                            colupper = 'black'
+
+                        title = kind_of_block
+
+                        begin_text = '\\begin{'+'tcolorbox'+'}[width=\\textwidth,colback={' + color + '},title={' + title + '},outer arc=0mm,colupper='+colupper+']'
+                        end_text = '\end{'+'tcolorbox'+'}'
                 else:
                     language_additive = ''
-                s1 = '\\begin{minted}'+language_additive
+                    begin_text = begin_text_0
+
+
+                s1 = begin_text+language_additive
             else:
-                s1 = '\end{minted}'
+                s1 = end_text
 
             counter += 1
 
