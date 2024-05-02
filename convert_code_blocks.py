0cbff5ade1e7f447162a0ab9d14873b7b0d45f61
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2024-05-02 13:42:28 +0200

    Update convert_code_blocks.py

--- convert_code_blocks.py
+++ convert_code_blocks.py
@@ -3,7 +3,7 @@ import os
 from remove_markdown_comment import *
 
 
-def code_block_converter(S):
+def code_block_converter(S, PARS):
 
     if not isinstance(S, list):
         raise Exception('The input needs to be a list!')
@@ -14,6 +14,8 @@ def code_block_converter(S):
     counter = 0
     S1 = []
     code_block_obsidian = '```' 
+
+    settings__admonition = PARS['⚙']['code_blocks']['admonition']
     for i, s in enum(S):
         s1 = s
         if s1.startswith(code_block_obsidian):
@@ -22,25 +24,52 @@ def code_block_converter(S):
                 language = s1.replace(code_block_obsidian, '').lstrip().rstrip()
 
                 if language:
+
                     IS_ADMONITION_BLOCK = language.startswith('ad-')
+                    
                     if not IS_ADMONITION_BLOCK:
                         language_additive = '{' + language + '}'
                         begin_text =  begin_text_0
                         end_text = end_text_0
                     else:
                         language_additive = ''
-                        kind_of_block = language.replace('ad-', '').strip()
-                        if kind_of_block == 'warning':
-                            color = 'red'
-                            colupper = 'white'
-                        else:
-                            color = 'white'
-                            colupper = 'black'
+                        idx_kind_default = 0
 
+
+                        kind_of_block = language.replace('ad-', '').strip()
+                        block_kinds = [x[0] for x in settings__admonition]
+
+                        idx_kind = -10
+                        for j, b in enumerate(block_kinds):
+                            if kind_of_block == b:
+                                idx_kind = j
+                                break
+
+                        if idx_kind==-10:
+                            idx_kind = idx_kind_default
+                        
+                        settings__admonition_block = settings__admonition[idx_kind][1]
+                        
+                        color    = settings__admonition_block[0]
+                        colupper = settings__admonition_block[1]
                         title = kind_of_block
 
                         begin_text = '\\begin{'+'tcolorbox'+'}[width=\\textwidth,colback={' + color + '},title={' + title + '},outer arc=0mm,colupper='+colupper+']'
                         end_text = '\end{'+'tcolorbox'+'}'
+
+                        # Check for observation block
+                        if kind_of_block == 'attention':
+                            j = i+1
+                            while True:
+                                if S[j]:
+                                    if not 'Observation' in S[j]:
+                                        break
+                                    else:
+                                        S[j] = '{\Large \\textbf{Observation}}\n\n'
+                                        S[j+1] = ''
+                                        break
+                        
+                                        
                 else:
                     language_additive = ''
                     begin_text = begin_text_0
