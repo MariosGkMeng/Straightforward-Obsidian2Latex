b12759caf00ab8d55cc894d64e8e61e21b31e92d
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2024-08-01 15:33:40 +0200

    Update convert_code_blocks.py
    Added the capability of writing pure latex code within the obsidian note

--- src/convert_code_blocks.py
+++ src/convert_code_blocks.py
@@ -19,70 +19,74 @@ def code_block_converter(S, PARS):
     for i, s in enum(S):
         s1 = s
         if s1.startswith(code_block_obsidian):
-            if counter%2 == 0:
-                # we are at the start
+            code_block_has_started = counter%2 == 0
+            if code_block_has_started:
                 language = s1.replace(code_block_obsidian, '').lstrip().rstrip()
 
                 if language:
-
-                    IS_ADMONITION_BLOCK = language.startswith('ad-')
-                    
-                    if not IS_ADMONITION_BLOCK:
-                        language_additive = '{' + language + '}'
-                        begin_text =  begin_text_0
-                        end_text = end_text_0
-                    else:
+                    if language=='latex':
                         language_additive = ''
-                        idx_kind_default = 0
-
-
-                        kind_of_block = language.replace('ad-', '').strip()
-                        block_kinds = [x[0] for x in settings__admonition]
-
-                        idx_kind = -10
-                        for j, b in enumerate(block_kinds):
-                            if kind_of_block == b:
-                                idx_kind = j
-                                break
-
-                        if idx_kind==-10:
-                            idx_kind = idx_kind_default
+                        begin_text = ''
+                        end_text = ''
+                    else:
                         
-                        settings__admonition_block = settings__admonition[idx_kind][1]
+                        IS_ADMONITION_BLOCK = language.startswith('ad-')
                         
-                        color    = settings__admonition_block[0]
-                        colupper = settings__admonition_block[1]
-                        title = kind_of_block
+                        if not IS_ADMONITION_BLOCK:
+                            language_additive = '{' + language + '}'
+                            begin_text =  begin_text_0
+                            end_text = end_text_0
+                        else:
+                            language_additive = ''
+                            idx_kind_default = 0
 
-                        end_text = '\end{'+'tcolorbox'+'}'
 
-                        # Check for observation block
-                        if kind_of_block == 'attention':
-                            t_type = 'Observation'
-                            j = i+1
-                            while True:
-                                if S[j]:
-                                    if not t_type in S[j]:
-                                        break
-                                    else:
-                                        S[j] = '{\Large \\textbf{' + t_type + '}}\n\n\\newline'
-                                        S[j+1] = ''
-                                        title = ' '
-                                        color = 'cyan'
-                                        break
-                        
-                        begin_text =\
-                            '\\begin{'+\
-                            'tcolorbox'+\
-                            '}[width=\\textwidth,colback={' +\
-                            color +\
-                            '},title={' +\
-                            title + '},outer arc=0mm,colupper='+colupper+']'
+                            kind_of_block = language.replace('ad-', '').strip()
+                            block_kinds = [x[0] for x in settings__admonition]
+
+                            idx_kind = -10
+                            for j, b in enumerate(block_kinds):
+                                if kind_of_block == b:
+                                    idx_kind = j
+                                    break
+
+                            if idx_kind==-10:
+                                idx_kind = idx_kind_default
+                            
+                            settings__admonition_block = settings__admonition[idx_kind][1]
+                            
+                            color    = settings__admonition_block[0]
+                            colupper = settings__admonition_block[1]
+                            title = kind_of_block
+
+                            end_text = '\end{'+'tcolorbox'+'}'
+
+                            # Check for observation block
+                            if kind_of_block == 'attention':
+                                t_type = 'Observation'
+                                j = i+1
+                                while True:
+                                    if S[j]:
+                                        if not t_type in S[j]:
+                                            break
+                                        else:
+                                            S[j] = '{\Large \\textbf{' + t_type + '}}\n\n\\newline'
+                                            S[j+1] = ''
+                                            title = ' '
+                                            color = 'cyan'
+                                            break
+                            
+                            begin_text =\
+                                '\\begin{'+\
+                                'tcolorbox'+\
+                                '}[width=\\textwidth,colback={' +\
+                                color +\
+                                '},title={' +\
+                                title + '},outer arc=0mm,colupper='+colupper+']'
 
                 else:
                     language_additive = ''
                     begin_text = begin_text_0
-
 
                 s1 = begin_text+language_additive
             else:
