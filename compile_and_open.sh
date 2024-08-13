2d602045dac3a08419a795d2a73a47143fd8a3c2
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2024-08-13 19:11:36 +0200

    Create compile_and_open.sh

---
+++ compile_and_open.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This file compiles the latex file to .pdf
+
+# Set base path and file name
+BASE_PATH="C:/Users/mariosg/OneDrive - NTNU/FILES/workTips/✍Writing"
+FILE_NAME="✍⌛writing--PHEN--Mathematical Phenomena of Neural Networks"
+TEXFILE="$BASE_PATH/$FILE_NAME.tex"
+PDFFILE="$BASE_PATH/$FILE_NAME.pdf"
+
+# Print paths for debugging
+echo "TEXFILE: $TEXFILE"
+echo "PDFFILE: $PDFFILE"
+
+# Compile the LaTeX file
+pdflatex -interaction=nonstopmode -shell-escape "$TEXFILE"
+if [ $? -ne 0 ]; then
+    echo "pdflatex compilation failed."
+    exit 1
+fi
+
+# Open the resulting PDF file
+xdg-open "$PDFFILE"
