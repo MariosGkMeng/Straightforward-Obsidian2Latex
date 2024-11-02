f8f383f4b1cd1ffdcd18b308b830de2ba5554717
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2024-11-02 17:49:21 +0100

    Update symbol_replacements.py

--- src/symbol_replacements.py
+++ src/symbol_replacements.py
@@ -1,16 +1,15 @@
 import re
 
-def symbol_replacement(S, PARS):
-    
-    SYMBOLS_TO_REPLACE = PARS['par']['symbols-to-replace']
+def symbol_replacement(S, SYMBOLS_TO_REPLACE):
     
     S_1 = []
     for s in S:
         s1 = s
         for symbol in SYMBOLS_TO_REPLACE:
-            if symbol[2] == 1:
+            s2 = symbol[2]
+            if s2 == 1:
                 s1 = s1.replace(symbol[0], symbol[1] + ' ')
-            elif symbol[2] == 2:
+            elif s2 == 2:
                 s1 = re.sub(symbol[0], symbol[1] + ' ', s1)
             else:
                 raise Exception("Nothing coded for this case!")
