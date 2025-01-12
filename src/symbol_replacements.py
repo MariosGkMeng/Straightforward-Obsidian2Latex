1a8b36fe5f41946a73e725b7ec7345f0341cc343
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2025-01-12 21:39:18 +0100

    Update symbol_replacements.py
    Added function: escape_underscores_in_texttt

--- src/symbol_replacements.py
+++ src/symbol_replacements.py
@@ -7,7 +7,9 @@ def symbol_replacement(S, SYMBOLS_TO_REPLACE):
         s1 = s
         for symbol in SYMBOLS_TO_REPLACE:
             s2 = symbol[2]
-            if s2 == 1:
+            if s2 == 0:
+                s1 = s1.replace(symbol[0], symbol[1])
+            elif s2 == 1:
                 s1 = s1.replace(symbol[0], symbol[1] + ' ')
             elif s2 == 2:
                 s1 = re.sub(symbol[0], symbol[1] + ' ', s1)
@@ -17,3 +19,27 @@ def symbol_replacement(S, SYMBOLS_TO_REPLACE):
         S_1.append(s1)
 
     return S_1
+
+
+def escape_underscores_in_texttt(text):
+    """
+    Replaces underscores with \_ inside the brackets of \texttt{}.
+
+    Args:
+        text (str): The input LaTeX string.
+
+    Returns:
+        str: The modified LaTeX string with escaped underscores inside \texttt{},
+             or the original string if no match is found.
+    """
+    def replace_underscores(match):
+        # Escape underscores in the matched text (inside \texttt{})
+        return match.group(0).replace('_', r'\_')
+
+    # Regex pattern to find \texttt{...}
+    pattern = r'\\texttt\{.*?\}'
+    
+    # Replace underscores only within \texttt{...} if any matches exist
+    replaced_text = re.sub(pattern, replace_underscores, text)
+    
+    return replaced_text
