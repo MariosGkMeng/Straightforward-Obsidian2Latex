d340709e3ce17654087cfb70cdd2290208383b64
Marios Gkionis <61937432+MariosGkMeng@users.noreply.github.com>
2026-01-12 09:48:32 +0100

    Update special characters in get_special_characters function

--- src/special_characters.py
+++ src/special_characters.py
@@ -1,4 +1,4 @@
 def get_special_characters():
-    SPECIAL_CHARACTERS = r" ,':?%💬💭⚠✍⌛💼🟢🧮➕❓⛏🔭❌👆🔴⭐✔🧑☺📁🗣⚙🔒🤔🟡🔲💊💡🤷‍♂️▶📧🔗🎾👨‍💻📞💭📖ℹ🤖🏢🧠🕒👇📚👉0-9\(\)\(\)\.\-\s"
+    SPECIAL_CHARACTERS = r" ,':?%💬💭⚠✍⌛🙁💼🟢🧮➕❓⛏🔭❌👆🔴⭐✔🧑☺📁🏗🗣⚙🔒🤔🟡🔲💊💡🤷‍♂️▶📧🔗🎾🧪👨‍💻📞💭📖ℹ🤖🏢🧠🕒👇📚👉0-9\(\)\.\-\s\\"
     
     return SPECIAL_CHARACTERS
